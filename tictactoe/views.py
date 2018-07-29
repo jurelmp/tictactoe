436bac684ce05b8b93bc1a418fe52b664c52cc01
Jurel P <patocjurel@gmail.com>
2018-07-29 23:18:23 +0800

    Global scope of static files and templates

--- tictactoe/views.py
+++ tictactoe/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def welcome(request):
-    return HttpResponse("Hello World from Django!")
+    return render(request, 'tictactoe/welcome.html')
