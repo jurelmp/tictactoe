9bb7b43b0ddc741dd8fdc5079bc52bcc2ca8410a
patocjurel <patoc.jm@sprobe.com>
2018-07-30 16:53:23 +0800

    Sending of invitations to other players

--- player/models.py
+++ player/models.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Invitation(models.Model):
+    from_user = models.ForeignKey(User, related_name="invitations_sent")
+    to_user = models.ForeignKey(User, related_name="invitations_received")
+
+    message = models.CharField(max_length=300)
+    timestamp = models.DateTimeField(auto_now_add=True)
